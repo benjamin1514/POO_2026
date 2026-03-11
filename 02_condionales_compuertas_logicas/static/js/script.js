1512b34c60293b04dd993042d349a5a135c5bc76
benjamin1514 <benjamindelgado@liceovvh.cl>
2026-03-11 14:23:09 -0300

    .

--- 02_condionales_compuertas_logicas/static/js/script.js
+++ 02_condionales_compuertas_logicas/static/js/script.js
@@ -4,7 +4,7 @@ console.log("Js conectado...");
 
 function porteroDigital() {
     let edad = parseInt(prompt("Ingrese su edad"));
-    if(edad >= 18) {
+    if (edad >= 18) {
         alert("Acceso concedido, eres mayor de edad")
     } else {
         alert("Acceso denegado, eres menor de edad")
@@ -15,14 +15,14 @@ function porteroDigital() {
 /*Declara una variable nombre.
 Si el nombre no está vacío, crea un arreglo llamado usuarios y agrega el nombre usando .push().
 Si está vacío, muestra: "Error: El nombre no puede estar en blanco".
-*/ 
+*/
 
 function nombre() {
     let nombre = "hola1234"
     let usuarios = [];
     usuarios.push(`${nombre}`);
-    
-    if(nombre && usuarios) {
+
+    if (nombre && usuarios) {
         alert(`Bienvenido ${usuarios}`);
     } else {
         alert("Error: el nombre no puede estar en blanco");
@@ -40,7 +40,7 @@ function filtroDuplicados() {
     let productos = ["celular", "teclado", "mouse"];
     let nuevoProducto = "celular";
 
-    if(productos.includes(nuevoProducto)) {
+    if (productos.includes(nuevoProducto)) {
         alert("El producto ya está en el inventario")
     } else {
         alert("El producto no esta en el inventario")
@@ -51,11 +51,11 @@ function filtroDuplicados() {
 let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
 Si el tamaño de la bodega (.length) es mayor a 5, elimina la última caja con .pop() y muestra: "Bodega llena, eliminando último ingreso".
 Si es 5 o menos, muestra: "Espacio disponible".
-*/ 
+*/
 
 function stock() {
     let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
-    if(bodega.length > 5) {
+    if (bodega.length > 5) {
         bodega.pop();
         alert("Bodega llena, eliminando último ingreso");
     } else {
@@ -74,10 +74,10 @@ function limpiezaDatos() {
     let colaEspera = ["error_404", "Juan", "Sofía"];
     let indice0 = colaEspera.shift();
 
-    if(indice0 === colaEspera) {
+    if (indice0 === colaEspera) {
         alert(`${colaEspera}`)
     } else {
-        alert(`La lista de espera esta correcta ${colaEspera }`)
+        alert(`La lista de espera esta correcta ${colaEspera}`)
     }
 };
 
@@ -89,12 +89,73 @@ Si no es Admin, agrégalo al final con .push(). */
 
 function VIP() {
     let foro = ["User1", "User2"];
-    let rol = "admin";
-
-    if(foro[0] === rol && foro.unshift) {
-        alert(`${foro[1]}`)
+    let usuarioNuevo = prompt("Ingrese un usuario: ")
+    if (usuarioNuevo.toLowerCase() === "admin") {
+        foro.unshift(usuarioNuevo);
+        alert(foro)
     } else {
-        alert(`${foro[0].shift()}`)
-    }
+        foro.push(usuarioNuevo);
+        alert(foro)
+    };
 
+};
+
+
+/*7. Sistema de Calificaciones Chile (Escala 1 a 7)
+Crea una variable nota.
+Si la nota es 6 o superior: "Excelente, eximido".
+Si la nota es entre 4 y 5.9: "Aprobado, vas a examen".
+Si es menor a 4: "Reprobado".
+(Pista: Usa else if). */
+
+function calificaciones() {
+    let nota = parseFloat(prompt("Ingrese su nota: "))
+    
+    if(nota >= 6 && nota <= 7) {
+        alert("Excelente eximido")
+    } else if (nota >= 4 && nota <= 5.9) {
+        alert("Aprobado, vas a examen")
+    } else if (nota < 4 && nota >= 1) {
+        alert("Reprobado")
+    } else {
+        alert("Coloque una nota valida.")
+    }
+}
+
+/* let invitados = ["Ana", "Luis", "Camila", "Diego"];
+Crea una variable persona.
+Si la persona está en la lista, muestra: "¡Bienvenido/a [nombre]! Pasa a la fiesta".
+Si no está, muestra: "Lo siento, no estás en la lista de invitados".
+*/
+
+function invitados() {
+    let invitados = ["Ana", "Luis", "Camila", "Diego"];
+
+    let persona = prompt("Coloque su nombre: ");
+    if(invitados.includes(persona)) {
+        alert(`¡Bienvenido ${persona}! Pasa a la fiesta`);
+    } else {
+        alert("Lo siento, no estás en la lista de invitados.")
+    }
+};
+
+
+/*Imagina una estantería con dos niveles (una matriz):
+let estanteria = [ ["Manzanas", "Peras"], ["Leche", "Yogur"] ];
+Crea una variable seccion (0 o 1) y una variable producto.
+Accede a la sección elegida. Si el producto solicitado está en esa sub-lista, muestra: "Producto encontrado en el estante".
+Si no, muestra: "No tenemos ese producto en esa sección".
+ */
+
+function almacenMatrices() {
+    let estanteria = [ ["Manzanas", "Peras"], ["Leche", "Yogur"] ];
+    let seccion = prompt("Ingrese 0 o 1")
+    let producto = prompt("que producto quiere");
+    
+
+    if(seccion.includes(estanteria)) {
+        alert(`El producto esta en la estanteria`)
+    } else {
+        alert("No tenemos ese producto en esa sección")
+    }
 }
