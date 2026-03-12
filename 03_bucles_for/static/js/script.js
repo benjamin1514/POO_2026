14b5626b9292e60aa82e24c972e69df9a0b45e26
benjamin1514 <benjamindelgado@liceovvh.cl>
2026-03-12 11:13:22 -0300

    .

--- 03_bucles_for/static/js/script.js
+++ 03_bucles_for/static/js/script.js
@@ -30,8 +30,44 @@ function asistenciaAutomatica() {
     const resultado = document.getElementById('result2');
 
     for (let i = 0; i <= 3; i++) {
-        curso.includes(resultado)
-    }
+        resultado.textContent += `${curso[i]} \n`
+        console.log(curso[i])
+    };
+
     resultado.textContent = `${curso.join(", ")}`;
     container.classList.remove('d-none');
 };
+
+
+/* Tienes una lista con las notas finales de un curso: let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+Crea una variable contadorAprobados que inicie en 0.
+Recorre el arreglo con un for. Si la nota es 4.0 o superior, suma 1 al contador.
+Al terminar el ciclo, muestra en el párrafo pantalla: "Total de alumnos aprobados: [número]".
+ */
+
+function aprobados() {
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+    const container = document.getElementById('resultadoContainer3');
+    const resultado = document.getElementById('result3');
+
+    let contadorAprobados = 0;
+
+    for (let i = 0; i <= notas.length; i++) {
+        if (notas[i] >= 4) {
+            contadorAprobados++
+        } 
+    };
+    resultado.textContent = `Total de los alumnos aprobados: ${contadorAprobados}`
+    container.classList.remove('d-none');
+};
+
+/*Una tienda quiere mostrar solo los productos que están disponibles.
+let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+Recorre la lista con un ciclo for.
+Si el producto es diferente de "Agotado" (!==), 
+agrégalo al texto del párrafo pantalla. Si dice "Agotado", sáltalo.
+ */
+
+function inventario() {
+    let productos = ["Teclado", "Mouse", "Agotado", "Monitor", "Agotado", "Audífonos"];
+}
